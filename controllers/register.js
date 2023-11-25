a7821d6125a11d9337487aaa188445f4c4ffd146
partha2001ps <parthapn2017@gmail.com>
2023-11-25 12:18:28 +0530

    comit

--- controllers/register.js
+++ controllers/register.js
@@ -5,8 +5,7 @@ const userRouter = require('express').Router();
 
 userRouter.post('/', async (req, res) => {
     const { username, name, password } = req.body;
-    const existingUser = await user.findOne({ username }).maxTimeMS(30000);
-
+    const existingUser = await user.findOne({ username }).maxTimeMS(30000).lean();  
     if (existingUser) {
         return res.json({
              message: 'This email ID is already in use. Please choose another one.' 
