7a5b60c418269272c4cd6fa67bee9ac3b0c65928
partha2001ps <parthapn2017@gmail.com>
2023-11-25 13:34:25 +0530

    config.js

--- utilies/config.js
+++ utilies/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://parthasarathigovind2001:Partha2001@cluster0.d9xtwo1.mongodb.net/password';
-const JWT_SECRET = process.env.JWT_SECRET || 'partha';
-const OTP = process.env.OTP || 'zceeslzyuudjptea';
+const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+const OTP = process.env.OTP;
 
 module.exports = {
     PORT,
